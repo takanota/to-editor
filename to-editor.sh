c3c6b7bbefc26e11a819d393d94f1c8c66525694
Takahiro Takano <3506204+takanota@users.noreply.github.com>
2018-10-13 15:00:24 +0900

    add -e (editor path) option

--- to-editor.sh
+++ to-editor.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 set -u
-VERBOSE=N
 
-while getopts "v" OPT; do
+VERBOSE=N
+[ -v EDITOR ] || EDITOR=gedit
+
+err() {
+  echo "$@" >/dev/stderr
+}
+
+while getopts ":e:v" OPT; do
   case "$OPT" in
+  e)
+    EDITOR="$OPTARG"
+    ;;
   v)
     VERBOSE="Y"
     ;;
   esac
 done
+
+EDITOR_PATH=$(which $EDITOR)
+if [ -z "$EDITOR_PATH" ]; then
+  err "editor is not exists: $EDITOR"
+  exit 1
+elif [ ! -x "$EDITOR_PATH" ]; then
+  err "editor is not executable: $EDITOR"
+  exit 1
+fi
 
 MKTEMP_OPT="--suffix .txt stdin.XXXXXXXXXX"
 if [ -w . ]; then
@@ -19,18 +37,21 @@ TEMPNAME=$(mktemp $MKTEMP_OPT)
 on_exit() {
   sleep 5
   if [ "$VERBOSE" == "Y" ]; then
-    rm -v $TEMPNAME
+    rm -v "$TEMPNAME"
   else
-    rm $TEMPNAME
+    rm "$TEMPNAME"
   fi
 }
 trap 'on_exit' EXIT
 
 if [ "$VERBOSE" == "Y" ]; then
-  tee $TEMPNAME
+  tee "$TEMPNAME"
   echo "--> $TEMPNAME"
 else
-  cat $TEMPNAME
+  cat "$TEMPNAME"
 fi
 
-[ $? -eq 0 ] && atom $TEMPNAME
+if [ $? -eq 0 ]; then
+  echo "starting $EDITOR $TEMPNAME ..."
+  "$EDITOR" "$TEMPNAME" 1>/dev/null 2>/dev/null &
+fi
