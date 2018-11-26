042de92d9a263c2f410af738d78288af28066752
Takahiro Takano <3506204+takanota@users.noreply.github.com>
2018-11-27 06:17:53 +0900

    add --clear=N option

--- to-editor.sh
+++ to-editor.sh
@@ -8,6 +8,8 @@ usage() {
   echo "  -e, --editor=EDITOR editor path"
   echo "  -d, --tmpdir=DIR    directory to create temporary file"
   echo "                      (default: current directory)"
+  echo "  -c, --clear=N       remove temporary file after N second(s)"
+  echo "                      (0 to skip remove temporary file)"
   echo "  -h, --help          show this help"
   echo "  -v, --version       verbose output"
   echo ""
@@ -20,6 +22,7 @@ err() {
 VERBOSE=N
 EDITOR_PATH=
 TEMPDIR=.
+CLEAR_SECONDS=5
 
 while [ -n "$1" ]; do
   case "$1" in
@@ -36,6 +39,13 @@ while [ -n "$1" ]; do
     ;;
   --tmpdir=*)
     TEMPDIR=${1#*=}
+    ;;
+  -c|--clear)
+    shift
+    CLEAR_SECONDS=$1
+    ;;
+  --clear=*)
+    CLEAR_SECONDS=${1#*=}
     ;;
   -h|--help)
     usage
@@ -57,21 +67,29 @@ if [ -z "$EDITOR_PATH" ]; then
   err "--editor=EDITOR is required"
   usage
   exit 3
-elif [ ! -x "$EDITOR_PATH" ]; then
+fi
+if [ ! -x "$EDITOR_PATH" ]; then
   err "editor is not executable: $EDITOR_PATH"
   exit 4
+fi
+if [[ ! "$CLEAR_SECONDS" =~ ^[0-9]+$ ]]; then
+  err "--clear=N must be numeric"
+  usage
+  exit 5
 fi
 
 TEMPNAME=$(mktemp $TEMPDIR/stdin.XXXXXXXXXX.txt) || exit $?
 on_exit() {
-  sleep 5
+  sleep $CLEAR_SECONDS
   if [ "$VERBOSE" == "Y" ]; then
     rm -v "$TEMPNAME"
   else
     rm "$TEMPNAME"
   fi
 }
-trap 'on_exit &' EXIT
+if [ $CLEAR_SECONDS -gt 0 ]; then
+  trap 'on_exit &' EXIT
+fi
 
 if [ "$VERBOSE" == "Y" ]; then
   tee "$TEMPNAME"
